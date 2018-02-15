347ef7cd2b326253bcc2c2a6b9d685504ce2472b
Michael London <michael.london@wework.com>
2018-02-15 07:11:27 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,5 +12,13 @@ def input_to_index(index)
 end
 
 def valid_move?(board, index)
-  if board[index] == ""
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    if index > 0 && index < 9
+      return true
+    else
+      return false 
+    end
+  else
+    return false
+  end
 end
